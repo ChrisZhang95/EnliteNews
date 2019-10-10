ffc27f00f22c303a48ef71d215f22e50d2c0e800
ChrisZhang95 <chris.zhang95@hotmail.com>
2019-10-09 20:22:56 -0400

    included backend node folder

---
+++ node-enlite/server.js
@@ -0,0 +1,47 @@
+
+// libraries
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const NewsAPI = require('newsapi');
+
+// constants
+const constants = require('./constants');
+const NEWS_API_KEY = constants.NEWS_API_KEY;
+const newsapi = new NewsAPI(NEWS_API_KEY);
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  console.log("it is working!!!");
+  res.json('it is working!!!')
+})
+
+app.post('/news', (req, res) => {
+  var { category } = req.body;
+  console.log(req.body);
+  if (category === '') category = 'general';
+  newsapi.v2.topHeadlines({
+    category: category,
+    language: 'en',
+    country: 'us'
+  }).then(response => {
+    //console.log(response);
+    res.json(response);
+  }).catch(err => res.status(400).json(err));
+})
+
+
+app.post('/signin', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  console.log(username + ': ' + password);
+  res.json(username + ":" + password);
+
+})
+// process.env.PORT || 
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`app is runnning on port ${process.env.PORT}`);
+});
