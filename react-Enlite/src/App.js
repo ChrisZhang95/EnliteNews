3594cbb4b41ef2f9c3e90780aad53e504ba3c454
Chris Zhang <chris.zhang95@hotmail.com>
2019-10-23 15:21:12 -0400

    created prototype authentication page

--- react-Enlite/src/App.js
+++ react-Enlite/src/App.js
@@ -4,13 +4,15 @@ import Header from "./components/Header/header.component";
 
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/home.page';
-
+import AuthenticationPage from './pages/AuthenticationPage/authentication.page'
 function App() {
   return (
     <div className="App">
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path='/signin' component={AuthenticationPage} />
+        <Route exact path='/signup' component={AuthenticationPage} />
       </Switch>
 
     </div>
---
+++ react-Enlite/src/pages/AuthenticationPage/authentication.page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AuthenticationPage = () => (
+    <div>sign in/ up</div>
+)
+
+export default AuthenticationPage
